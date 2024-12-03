f16d81327f7485499f063fab8bde2d8349dc69a1
loick walle <loickwalle@gmail.com>
2024-12-03 14:03:54 +0100

    Refacto exercice2

--- Exercice02/exercice2/src/Main.java
+++ Exercice02/exercice2/src/Main.java
@@ -5,168 +5,174 @@ import models.Magazine;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    static List<LibraryItem> library = new ArrayList<>();
-    static List<BorrowRecord> borrowRecordList = new ArrayList<>();
-    static Scanner sc = new Scanner(System.in);
+    private static final List<LibraryItem> library = new ArrayList<>();
+    private static final List<BorrowRecord> borrowRecordList = new ArrayList<>();
+    private static final Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-
         initLibrary();
-        System.out.println("\nWelcome to the jungle library !");
-        do{
-            System.out.print("""
-            Select an option :
+        System.out.println("\nWelcome to the Jungle Library!");
+
+        while (true) {
+            displayMainMenu();
+
+            int option = getUserNumberInput();
+
+            if (option == 5) {
+                System.out.println("Thanks for using the library!");
+                break;
+            }
+
+            menuOption(option);
+        }
+    }
+
+    private static void displayMainMenu() {
+        System.out.print("""
+            
+            Select an option:
             1) Add an element
             2) Display all elements
             3) Borrow an element
-            4) Display available book
+            4) Display item status
             5) Quit
-            
-            Your choice :
-            """);
-
-            int option = sc.nextInt();
-            sc.nextLine();
-
-            switch (option){
-                case 1 -> createLibraryItem();
-                case 2 -> displayAllElements();
-                case 3 -> borrowElement();
-                case 4 -> displayItemStatus();
-            }
-
-            if(option == 5){
-                System.out.println("Thanks for using the library !");
-                break;
-            }
-
-        }while(true);
+            Your choice: """);
     }
 
-    public static void displayAllElements(){
-        System.out.println("\nAll elements : ");
-        for(LibraryItem libraryItem : library){
+    private static int getUserNumberInput() {
+        int option = sc.nextInt();
+        sc.nextLine();  // Consume the newline
+        return option;
+    }
+
+    private static void menuOption(int option) {
+        switch (option) {
+            case 1 -> createLibraryItem();
+            case 2 -> displayAllElements();
+            case 3 -> borrowElement();
+            case 4 -> displayItemStatus();
+        }
+    }
+
+    private static void displayAllElements() {
+        System.out.println("\nAll elements:");
+        for (LibraryItem libraryItem : library) {
             System.out.println(libraryItem.getDetails());
         }
-        System.out.println();
     }
 
-    public static void displayItemStatus(){
+    private static void displayItemStatus() {
         displayAvailableItems();
-        System.out.println();
         displayBorrowedItems();
-        System.out.println();
     }
 
-    public static void displayAvailableItems(){
-        System.out.println("Available items : ");
-        for(LibraryItem libraryItem : library){
-            boolean isAvailable = true;
-
-            for(BorrowRecord record : borrowRecordList){
-                if (libraryItem.getId() == record.itemId()) {
-                    isAvailable = false;
-                    break;
-                }
+    private static void displayAvailableItems() {
+        System.out.println("\nAvailable items:");
+        for (LibraryItem libraryItem : library) {
+            if (isItemAvailable(libraryItem)) {
+                System.out.println(libraryItem.getDetails());
             }
-
-            if(isAvailable)
-                System.out.println(libraryItem.getDetails());
         }
     }
 
-    public static void displayBorrowedItems(){
-        System.out.println("Borrowed items : ");
-        for(BorrowRecord record : borrowRecordList){
+    private static boolean isItemAvailable(LibraryItem libraryItem) {
+        for (BorrowRecord record : borrowRecordList) {
+            if (libraryItem.getId() == record.itemId()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void displayBorrowedItems() {
+        System.out.println("\nBorrowed items:");
+        for (BorrowRecord record : borrowRecordList) {
             LibraryItem libraryItem = getItemInLibraryById(record.itemId());
-            if(libraryItem != null)
+            if (libraryItem != null) {
                 System.out.println(libraryItem.getDetails());
+            }
         }
     }
 
-    public static void borrowElement(){
+    private static void borrowElement() {
         displayAvailableItems();
 
-        System.out.println("Which item to you want to borrow ?");
-
-        int idChoiced = sc.nextInt();
+        System.out.println("Which item do you want to borrow?");
+        int idChoose = sc.nextInt();
         sc.nextLine();
 
-        System.out.println("Whats the borrower's name ?");
+        System.out.println("What's the borrower's name?");
         String borrowerName = sc.nextLine();
 
-        for(LibraryItem libraryItem : library){
-            if(idChoiced == libraryItem.getId())
-                borrowRecordList.add(new BorrowRecord(libraryItem.getId(), borrowerName , LocalDate.now().toString()));
-        }
+        borrowRecordList.add(new BorrowRecord(idChoose, borrowerName, LocalDate.now().toString()));
     }
 
-    public static void createLibraryItem(){
-        String title;
-        int publicationYear;
+    private static void createLibraryItem() {
+        System.out.print("""
+            What do you want to create:
+            1) Book
+            2) Magazine
+            Your choice: """);
+
+        int option = getUserNumberInput();
+        System.out.println("Title: ");
+        String title = sc.nextLine();
+
+        System.out.println("Publication year: ");
+        int publicationYear = sc.nextInt();
+        sc.nextLine();
+
         LibraryItem libraryItem = null;
 
-        System.out.print("""
-                What do you want to create :
-                1) Book ?
-                2) Magazine ?
-                
-                Your choice :
-                """);
-
-        int option = sc.nextInt();
-        sc.nextLine();
-
-        System.out.println("Title : ");
-        title = sc.nextLine();
-        System.out.println("Publication year : ");
-        publicationYear = sc.nextInt();
-        sc.nextLine();
-
-        switch (option){
-            case 1 :
-                System.out.println("Author : ");
-                String author = sc.nextLine();
-                System.out.println("Genre : ");
-                String genre = sc.nextLine();
-                libraryItem = new Book(title, publicationYear, author, genre);
-                break;
-            case 2 :
-                System.out.println("Number : ");
-                int number = sc.nextInt();
-                sc.nextLine();
-                libraryItem = new Magazine(title, publicationYear, number);
-                break;
+        switch (option) {
+            case 1 -> libraryItem = createBook(title, publicationYear);
+            case 2 -> libraryItem = createMagazine(title, publicationYear);
         }
 
-        if(libraryItem != null)
+        if (libraryItem != null) {
             addItemToLibrary(libraryItem);
-
+        }
     }
 
-    public static void initLibrary(){
-        Book book = new Book("Coder pour les nuls", 2024, "Pr.Hashtag", "SF");
-        Magazine magazine = new Magazine("Coder pour les nuls", 2024, 32);
-        addItemToLibrary(book);
-        addItemToLibrary(magazine);
+    private static Book createBook(String title, int publicationYear) {
+        System.out.println("Author: ");
+        String author = sc.nextLine();
+
+        System.out.println("Genre: ");
+        String genre = sc.nextLine();
+
+        return new Book(title, publicationYear, author, genre);
     }
 
-    public static void addItemToLibrary(LibraryItem libraryItem){
-        libraryItem.setId(library.size()+1);
+    private static Magazine createMagazine(String title, int publicationYear) {
+        System.out.println("Number: ");
+        int number = sc.nextInt();
+        sc.nextLine();  // Consume newline
+
+        return new Magazine(title, publicationYear, number);
+    }
+
+    private static void addItemToLibrary(LibraryItem libraryItem) {
+        libraryItem.setId(library.size() + 1);
         library.add(libraryItem);
         System.out.println("Element added to the library...");
     }
 
-    public static LibraryItem getItemInLibraryById(int id){
-        for(LibraryItem libraryItem : library){
-            if(libraryItem.getId() == id)
-                return libraryItem;
-        }
-        return null;
+    private static LibraryItem getItemInLibraryById(int id) {
+        return library.stream()
+                .filter(item -> item.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+
+    private static void initLibrary() {
+        Book book = new Book("Coder pour les nuls", 2024, "Pr.Hashtag", "SF");
+        Magazine magazine = new Magazine("Coder pour les nuls", 2024, 32);
+        addItemToLibrary(book);
+        addItemToLibrary(magazine);
     }
 }
