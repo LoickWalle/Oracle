38f2729affabd2059716f58be4c2ef4f191c194b
loick walle <loickwalle@gmail.com>
2024-12-04 16:10:10 +0100

    Refacto exercice2

--- Design Pattern/Exercice2/src/models/CurrencyConverter.java
+++ Design Pattern/Exercice2/src/models/CurrencyConverter.java
@@ -7,24 +7,18 @@ public class CurrencyConverter {
     private final double GBP_TO_USD = 1.3;
 
     public double convertToUSD(String currency, double amount) {
-        if(Objects.equals(currency, "EUR"))
-            return amount * EUR_TO_USD;
-        if(Objects.equals(currency, "GBP"))
-            return amount * GBP_TO_USD;
-        else{
-            System.out.println("Currency not available.");
-            return 0;
-        }
+        return switch (currency){
+            case "EUR" -> amount * EUR_TO_USD;
+            case "GBP" -> amount * GBP_TO_USD;
+            default -> amount;
+        };
     }
 
     public double convertFromUSD(String currency, double amount) {
-        if(currency.equals("EUR"))
-            return amount / EUR_TO_USD;
-        if(currency.equals("GBP"))
-            return amount / GBP_TO_USD;
-        else{
-            System.out.println("Currency not available.");
-            return 0;
-        }
+        return switch (currency){
+            case "EUR" -> amount / EUR_TO_USD;
+            case "GBP" -> amount / GBP_TO_USD;
+            default -> amount;
+        };
     }
 }
