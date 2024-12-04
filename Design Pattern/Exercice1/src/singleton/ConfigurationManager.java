dc655dd74512f6dce61e1946c98d519ceee03a18
loick walle <loickwalle@gmail.com>
2024-12-04 13:42:44 +0100

    Correction exercice1 (hash map immuable)

--- Design Pattern/Exercice1/src/singleton/ConfigurationManager.java
+++ Design Pattern/Exercice1/src/singleton/ConfigurationManager.java
@@ -1,11 +1,12 @@
 package singleton;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ConfigurationManager {
     private static ConfigurationManager instance;
-    private Map<String, String> configuration = new HashMap<>();
+    private static Map<String, String> configuration = new HashMap<>();
 
     private ConfigurationManager(){
         System.out.println("instance created !");
@@ -19,7 +20,10 @@ public class ConfigurationManager {
     }
 
     public void loadConfiguration(Map<String, String> configurationLoaded){
-        configuration = configurationLoaded;
+        if(!configuration.isEmpty())
+            return;
+
+        configuration = Collections.unmodifiableMap(configurationLoaded);
     }
 
     public String getValue(String key){
