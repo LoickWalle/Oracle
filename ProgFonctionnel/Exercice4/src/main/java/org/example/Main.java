a8c1a748533bc1d46e0222ef725ad38c2e19d60e
loick walle <loickwalle@gmail.com>
2024-12-11 11:28:05 +0100

    Correction exercice 4 (Stream)

--- ProgFonctionnel/Exercice4/src/main/java/org/example/Main.java
+++ ProgFonctionnel/Exercice4/src/main/java/org/example/Main.java
@@ -19,28 +19,34 @@ public class Main
         );
 
         //1
+        System.out.println("\nPartie 1 :");
         commandes.forEach(System.out::println);
 
         //2
+        System.out.println("\nPartie 2 :");
         commandes.stream()
                 .map(Commande::getClient)
                 .distinct()
                 .forEach(System.out::println);
 
         //3
+        System.out.println("\nPartie 3 :");
         System.out.println("Montant total : " + commandes.stream()
                 .mapToDouble(Commande::getMontantTotal)
                 .sum());
 
         //4
+        System.out.println("\nPartie 4 :");
         commandes.forEach(commande -> System.out.println("Articles : " + commande.getArticles() + " - Total : " + commande.getMontantTotal()));
 
         //5
+        System.out.println("\nPartie 5 :");
         commandes.stream()
                 .filter(Commande::isEstLivree)
                 .forEach(commande -> System.out.println("Commande livrée : " + commande));
 
         //6
+        System.out.println("\nPartie 6 :");
         commandes.stream()
                 .collect(Collectors.groupingBy(Commande::getClient))
                 .forEach((client, commandesByClient) -> {
@@ -48,16 +54,18 @@ public class Main
                             .mapToDouble(Commande::getMontantTotal)
                             .sum();
 
-                    System.out.print(client + " a dépensé : " + sumByClient);
+                    System.out.println(client + " a dépensé : " + sumByClient);
         });
 
         //7
+        System.out.println("\nPartie 7 :");
         commandes.stream()
                 .flatMap(commande -> commande.getArticles().stream())
                 .distinct()
                 .forEach(System.out::println);
 
         //8
+        System.out.println("\nPartie 8 :");
         commandes.stream()
                 .collect(Collectors.groupingBy(Commande::getClient))
                 .forEach((client, commandesClient) -> {
