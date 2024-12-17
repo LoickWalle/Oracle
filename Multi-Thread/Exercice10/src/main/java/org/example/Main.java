3dbe7e2d8ab5edaa34400bf7f9fa284a093c169e
loick walle <loickwalle@gmail.com>
2024-12-17 11:54:30 +0100

    Ajout exercice10 (atomic)

---
+++ Multi-Thread/Exercice10/src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Main {
+    private static AtomicLong atomicLong = new AtomicLong(1);
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread[] threads = new Thread[5];
+
+        for (int i = 0; i < 5; i++) {
+            int multiplier = i + 2 ;
+            threads[i] = new Thread(() -> {
+                long result = multiplier * atomicLong.get();
+                atomicLong.set(result);
+                System.out.println(Thread.currentThread().getName() + " multiplacateur : " + multiplier + " = " + result);
+            });
+        }
+        for (Thread thread : threads) thread.start();
+        for (Thread thread : threads) thread.join();
+        System.out.println("Valeur finale : " + atomicLong.get());
+    }
+}
