cb0c54894e303c3767fe1a5305dda1d5f1c0cca5
loick walle <loickwalle@gmail.com>
2024-12-17 11:33:12 +0100

    Ajout exercice7 (Atomic)

---
+++ Multi-Thread/Exercice7/src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Main {
+    private static AtomicInteger atomicInteger = new AtomicInteger(0);
+
+    public static void main(String[] args) throws InterruptedException {
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+
+        scheduler.scheduleWithFixedDelay(() -> {
+            System.out.println("Message périodique " + atomicInteger.incrementAndGet());
+            if(atomicInteger.get() >= 5){
+                scheduler.shutdown();
+                System.out.println("Programme terminé.");
+            }
+        }, 1, 2, TimeUnit.SECONDS);
+    }
+}
