aa65984e53475739cf077013b3d5829b4f402122
loick walle <loickwalle@gmail.com>
2024-12-18 14:43:18 +0100

    Ajout exercice16 : thread-safe tableau/list (Barrier)

--- Multi-Thread/Exercice16/src/main/java/org/example/Main.java
+++ Multi-Thread/Exercice16/src/main/java/org/example/Main.java
@@ -1,28 +1,40 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicIntegerArray;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         final int nbThread = 4;
 
         int[] numbers = new int[]{0,1,2,3,4,5,6,7};
-        List<Integer> partials = new ArrayList<>();
+        //List<Integer> partials = Collections.synchronizedList(new ArrayList<>());
+        AtomicIntegerArray partials = new AtomicIntegerArray(nbThread);
 
         CyclicBarrier barrier = new CyclicBarrier(nbThread, new Thread(() -> {
-            System.out.println("Somme totale : " + partials.stream().mapToInt(value -> value).sum());
+            //System.out.println("Somme totale : " + partials.stream().mapToInt(value -> value).sum());
+            int totalSum = 0;
+            for (int i = 0; i < nbThread; i++) {
+                totalSum += partials.get(i);
+            }
+            System.out.println("Somme totale : " + totalSum);
         }));
         Thread[] threads = new Thread[4];
 
         for (int i = 0; i < nbThread; i++) {
             int start = i*2;
             int end = (i*2)+1;
+            int finalI = i;
             threads[i] = new Thread(() -> {
                 try {
                     int result = numbers[start] + numbers[end];
-                    partials.add(result);
+                    //partials.add(result);
+                    partials.set(finalI, result);
+
                     System.out.println(Thread.currentThread().getName() + "a calculé une somme partielle de (indice : "+ start +" à indice : "+end+") : "+result);
                     barrier.await();
                 } catch (Exception e) {
