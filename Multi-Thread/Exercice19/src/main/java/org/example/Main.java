43de88a9ae077696c29be1aa4ec99d327020ed7d
loick <loickwalle@gmail.com>
2024-12-19 10:57:39 +0100

    Ajout exercice19 (ConcurrentLinkedQueue)

---
+++ Multi-Thread/Exercice19/src/main/java/org/example/Main.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        ConcurrentLinkedQueue<String> elements = new ConcurrentLinkedQueue<>();
+
+        Runnable addTask = () -> {
+            for (int i = 0; i < 10; i++) {
+                String element = Thread.currentThread().getName() + "-Element-"+i;
+                System.out.println(Thread.currentThread().getName() + " a ajouté : " + element);
+                elements.add(element);
+            }
+        };
+
+        Runnable removeTask = () -> {
+            for (int i = 0; i < 10; i++) {
+                if(elements.isEmpty())
+                    System.out.println(Thread.currentThread().getName() + " n'a trouvé aucun élément à retirer.");
+                else{
+                    String elementPick = elements.poll();
+                    System.out.println(Thread.currentThread().getName() + " a retiré : " + elementPick);
+                }
+            }
+        };
+
+        Thread thread = new Thread(addTask, "Producer");
+        Thread thread2 = new Thread(removeTask, "Consumer");
+
+        Thread[] threads = new Thread[]{thread, thread2};
+        runThreads(threads);
+
+        System.out.println("État final de la file : " + elements);
+    }
+
+    private static void runThreads(Thread[] threads) throws InterruptedException {
+        for (Thread thread : threads) thread.start();
+        for (Thread thread : threads) thread.join();
+    }
+
+}
