2ba14c3afd7f0bdf049e9f8eae05a0772ff47125
loick <loickwalle@gmail.com>
2025-01-20 16:37:30 +0100

    Correction exercice4

--- Spring WebFlux/Exercice4/src/main/java/org/example/exercice4/controller/ArticleController.java
+++ Spring WebFlux/Exercice4/src/main/java/org/example/exercice4/controller/ArticleController.java
@@ -1,5 +1,6 @@
 package org.example.exercice4.controller;
 
+import org.example.exercice4.entity.Article;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +12,9 @@ public class ArticleController {
 
     @GetMapping("/articles")
     public Flux<String> getArticles() {
-        return Flux.just("Introduction to Spring WebFlux"
-                , "Reactive Programming with Project Reactor"
-                , "Building APIs with Spring Boot");
+        return Flux.just(new Article(1, "Introduction to Spring WebFlux"),
+                new Article(2, "Reactive Programming with Project Reactor"),
+                new Article(3, "Building APIs with Spring Boot"))
+                .map(Article::getTitle);
     }
 }
